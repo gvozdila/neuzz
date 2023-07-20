c0adfefa7ce47147e823557e2fd69a5d499d0ad5
Yakunin Andrey <ayakunin@ippe.ru>
2023-07-20 23:04:28 +0300

    feat: grads seems to work good.
    Should change gen_adv3_pytorch too.
    After that test with server socket.

--- nn.py
+++ nn.py
@@ -7,18 +7,34 @@ import sys
 import glob
 import math
 import time
-import keras
+#import keras
 import random
 import socket
 import subprocess
 import numpy as np
-import tensorflow as tf
-import keras.backend as K
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as tt
+import torch
+import torch.nn as nn
+#import cv2
+#from tqdm.notebook import tqdm
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision.utils import save_image
+from torchvision.utils import make_grid
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns
+
+#import tensorflow as tf
+#import keras.backend as K
 from collections import Counter
-from tensorflow import set_random_seed
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.callbacks import ModelCheckpoint
+#from tensorflow import set_random_seed
+#from keras.models import Sequential
+#from keras.layers import Dense, Dropout, Activation
+#from keras.callbacks import ModelCheckpoint
 
 HOST = '127.0.0.1'
 PORT = 12012
@@ -31,13 +47,13 @@ round_cnt = 0
 seed = 12
 np.random.seed(seed)
 random.seed(seed)
-set_random_seed(seed)
+#set_random_seed(seed)
 seed_list = glob.glob('./seeds/*')
 new_seeds = glob.glob('./seeds/id_*')
 SPLIT_RATIO = len(seed_list)
 # get binary argv
 argvv = sys.argv[1:]
-
+device = "cpu"
 
 # process training data from afl raw data
 def process_data():
@@ -107,6 +123,9 @@ def process_data():
     for idx, i in enumerate(seed_list):
         file_name = "./bitmaps/" + i.split('/')[-1]
         np.save(file_name, fit_bitmap[idx])
+        
+    print("Data processed")
+    print("MAX_BITMAP_SIZE, MAX_FILE_SIZE",MAX_BITMAP_SIZE, MAX_FILE_SIZE)
 
 
 # training data generator
@@ -135,26 +154,40 @@ def step_decay(epoch):
     return lrate
 
 
-class LossHistory(keras.callbacks.Callback):
-
-    def on_train_begin(self, logs={}):
-        self.losses = []
-        self.lr = []
-
-    def on_epoch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
-        self.lr.append(step_decay(len(self.losses)))
-        print(step_decay(len(self.losses)))
+#class LossHistory(keras.callbacks.Callback):
+#
+#    def on_train_begin(self, logs={}):
+#        self.losses = []
+#        self.lr = []
+#
+#    def on_epoch_end(self, batch, logs={}):
+#        self.losses.append(logs.get('loss'))
+#        self.lr.append(step_decay(len(self.losses)))
+#        print(step_decay(len(self.losses)))
 
 
 # compute jaccard accuracy for multiple label
-def accur_1(y_true, y_pred):
-    y_true = tf.round(y_true)
-    pred = tf.round(y_pred)
-    summ = tf.constant(MAX_BITMAP_SIZE, dtype=tf.float32)
-    wrong_num = tf.subtract(summ, tf.reduce_sum(tf.cast(tf.equal(y_true, pred), tf.float32), axis=-1))
-    right_1_num = tf.reduce_sum(tf.cast(tf.logical_and(tf.cast(y_true, tf.bool), tf.cast(pred, tf.bool)), tf.float32), axis=-1)
-    return K.mean(tf.divide(right_1_num, tf.add(right_1_num, wrong_num)))
+# def accur_1(y_true, y_pred):
+#     y_true = tf.round(y_true)
+#     pred = tf.round(y_pred)
+#     summ = tf.constant(MAX_BITMAP_SIZE, dtype=tf.float32)
+#     wrong_num = tf.subtract(summ, tf.reduce_sum(tf.cast(tf.equal(y_true, pred), tf.float32), axis=-1))
+#     right_1_num = tf.reduce_sum(tf.cast(tf.logical_and(tf.cast(y_true, tf.bool), tf.cast(pred, tf.bool)), tf.float32), axis=-1)
+#     return K.mean(tf.divide(right_1_num, tf.add(right_1_num, wrong_num)))
+    
+# compute jaccard accuracy for multiple label
+# source https://torchmetrics.readthedocs.io/en/stable/classification/jaccard_index.html
+def accur_1_pytorch(y_true, y_pred ):
+    from torchmetrics.classification import MultilabelJaccardIndex
+#    y_tr = torch.round(y_true)
+#    pred = torch.round(y_pred)
+#    summ = MAX_BITMAP_SIZE
+#    metric = MultilabelJaccardIndex(num_labels=MAX_BITMAP_SIZE)
+    
+    # naive loss
+    return torch.sum((y_pred - y_true) * (y_pred - y_true))
+#    return metric(y_true,  y_pred)
+    
 
 
 def train_generate(batch_size):
@@ -260,8 +293,131 @@ def gen_adv2(f, fl, model, layer_list, idxx, splice):
     return adv_list
 
 
+def compute_loss_grad(model,  loss_fn,  sample, target):
+    """
+    Function 
+
+    @param model Model
+    @type pytorch model
+    @param loss_fn Loss function
+    @type pytorch loss func
+    @param sample Single input - vectrized file 
+    @type pytorch.tensor
+    @param target Single target for model
+    @type pytorch.tensor
+    @return Torch autograd 
+    @rtype    torch.autograd.grad for input sample
+    """
+    sample = torch.tensor(sample)
+    sample = sample.unsqueeze(0).to(device)  # prepend batch dimension for processing
+    sample.requires_grad == True
+    normalized_input = Variable(sample, requires_grad=True).to(device)
+    target = target.unsqueeze(0).to(device)
+    
+    model.zero_grad()
+    model.train()
+    prediction = model(normalized_input)
+    loss = loss_fn(prediction, target)
+    loss.backward(retain_graph=True)
+    
+    grad = normalized_input.grad
+    print("loss=", loss , "sample=", normalized_input, "grad=", normalized_input.grad , "detach,cpu,sq",  normalized_input.detach().cpu().squeeze())
+    # Do not need model grads, only grads w.r.t. input sample
+    return loss.item(),  grad.cpu().detach().numpy()[0] # grad.cpu().detach().numpy() shape is (1,1,MAX_FILE_SIZE) we'll turn it in (1,MAX_FILE_SIZE)
+
+    
+
+
+
+# compute gradient for given input
+def gen_adv2_pytorch(f, fl, model, loss_func, idxx, splice):
+    """
+           index =  of interesting feature,
+           fl = list of two random input seeds(with index idxx),
+           model,
+           layer_list = (layer.name, layer)  from model.layers
+            idxx = index of interested_indice
+            1 = WTF?
+    """
+    adv_list = []
+#    loss = layer_list[-2][1].output[:, f]
+    # Create np.array with targets shape (1,MAX_BITMAP_SIZE) - no need to target.unsqueeze(0) later. Or just pass Y[0]
+    bitmap = np.zeros((1, MAX_BITMAP_SIZE))
+    # Set idxx bit
+    bitmap[0][idxx-1] = 1
+    Y = torch.tensor(bitmap)
+
+#    grads = K.gradients(loss, model.input)[0]
+    
+    #iterate = K.function([model.input], [loss, grads])
+    ll = 2
+    while fl[0] == fl[1]:
+        fl[1] = random.choice(seed_list)
+
+    for index in range(ll):
+        x = vectorize_file(fl[index])
+#        loss_value, grads_value = iterate([x])
+        loss_value, grads_value = compute_loss_grad(model=model,  loss_fn=loss_func , sample= x, target=Y)
+        idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
+        val = np.sign(grads_value[0][idx])
+        adv_list.append((idx, val, fl[index]))
+        #print(("idx=", idx,"val=",  val, "fl=", fl[index]))
+
+    # do not generate spliced seed for the first round
+    if splice == 1 and round_cnt != 0:
+        if round_cnt % 2 == 0:
+            splice_seed(fl[0], fl[1], idxx)
+            x = vectorize_file('./splice_seeds/tmp_' + str(idxx))
+            #loss_value, grads_value = iterate([x])
+            loss_value, grads_value = compute_loss_grad(model=model,  loss_fn=loss_func , sample= x, target=Y)
+            idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
+            val = np.sign(grads_value[0][idx])
+            adv_list.append((idx, val, './splice_seeds/tmp_' + str(idxx)))
+        else:
+            splice_seed(fl[0], fl[1], idxx + 500)
+            x = vectorize_file('./splice_seeds/tmp_' + str(idxx + 500))
+            #loss_value, grads_value = iterate([x])
+            loss_value, grads_value = compute_loss_grad(model=model,  loss_fn=loss_func , sample= x, target=Y)
+            idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
+            val = np.sign(grads_value[0][idx])
+            adv_list.append((idx, val, './splice_seeds/tmp_' + str(idxx + 500)))
+    print(adv_list[0][0].shape, adv_list[0][1].shape)
+    return adv_list
+
+
 # compute gradient for given input without sign
 def gen_adv3(f, fl, model, layer_list, idxx, splice):
+    adv_list = []
+    loss = layer_list[-2][1].output[:, f]
+    grads = K.gradients(loss, model.input)[0]
+    iterate = K.function([model.input], [loss, grads])
+    ll = 2
+    while fl[0] == fl[1]:
+        fl[1] = random.choice(seed_list)
+
+    for index in range(ll):
+        x = vectorize_file(fl[index])
+        loss_value, grads_value = iterate([x])
+        idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
+        #val = np.sign(grads_value[0][idx])
+        val = np.random.choice([1, -1], MAX_FILE_SIZE, replace=True)
+        adv_list.append((idx, val, fl[index]))
+
+    # do not generate spliced seed for the first round
+    if splice == 1 and round_cnt != 0:
+        splice_seed(fl[0], fl[1], idxx)
+        x = vectorize_file('./splice_seeds/tmp_' + str(idxx))
+        loss_value, grads_value = iterate([x])
+        idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
+        # val = np.sign(grads_value[0][idx])
+        val = np.random.choice([1, -1], MAX_FILE_SIZE, replace=True)
+        adv_list.append((idx, val, './splice_seeds/tmp_' + str(idxx)))
+
+    return adv_list
+
+
+# compute gradient for given input without sign
+def gen_adv3_pytorch(f, fl, model, loss_func, idxx, splice):
     adv_list = []
     loss = layer_list[-2][1].output[:, f]
     grads = K.gradients(loss, model.input)[0]
@@ -319,7 +475,7 @@ def gen_mutate2(model, edge_num, sign):
 
     with open('gradient_info_p', 'w') as f:
         for idxx in range(len(interested_indice[:])):
-            # kears's would stall after multiple gradient compuation. Release memory and reload model to fix it.
+            # keras's would stall after multiple gradient compuation. Release memory and reload model to fix it.
             if idxx % 100 == 0:
                 del model
                 K.clear_session()
@@ -330,7 +486,13 @@ def gen_mutate2(model, edge_num, sign):
             print("number of feature " + str(idxx))
             index = int(interested_indice[idxx])
             fl = [rand_seed1[idxx], rand_seed2[idxx]]
-            adv_list = fn(index, fl, model, layer_list, idxx, 1)
+            adv_list = fn(index, fl, model, layer_list, idxx, 1) 
+#           index =  of interesting feature,
+#           fl = list of two random input seeds(with index idxx),
+#           model,
+#           layer_list = (layer.name, layer)  from model.layers
+#            idxx = index of interested_indice
+#            1 = WTF?
             tmp_list.append(adv_list)
             for ele in adv_list:
                 ele0 = [str(el) for el in ele[0]]
@@ -339,56 +501,188 @@ def gen_mutate2(model, edge_num, sign):
                 f.write(",".join(ele0) + '|' + ",".join(ele1) + '|' + ele2 + "\n")
 
 
-def build_model():
-    batch_size = 32
-    num_classes = MAX_BITMAP_SIZE
-    epochs = 50
+# grenerate gradient information to guide furture muatation
+def gen_mutate2_pytorch(model, loss_func,  edge_num, sign):
+    """
+    Function 
 
-    model = Sequential()
-    model.add(Dense(4096, input_dim=MAX_FILE_SIZE))
-    model.add(Activation('relu'))
-    model.add(Dense(num_classes))
-    model.add(Activation('sigmoid'))
+    @param model NN model for grads calculation
+    @type pytorch.model
+    @param loss_func Loss function
+    @type pytorch Loss
+    @param edge_num Number of seeds to work with
+    @type int
+    @param sign Sign if currently in train mod (data[:5] == b"train")
+    @type Boolean
+    """
+    tmp_list = []
+    # select seeds
+    print("#######debug" + str(round_cnt))
+    if round_cnt == 0:
+        new_seed_list = seed_list
+    else:
+        new_seed_list = new_seeds
 
-    opt = keras.optimizers.adam(lr=0.0001)
+    if len(new_seed_list) < edge_num:
+        rand_seed1 = [new_seed_list[i] for i in np.random.choice(len(new_seed_list), edge_num, replace=True)]
+    else:
+        rand_seed1 = [new_seed_list[i] for i in np.random.choice(len(new_seed_list), edge_num, replace=False)]
+    if len(new_seed_list) < edge_num:
+        rand_seed2 = [seed_list[i] for i in np.random.choice(len(seed_list), edge_num, replace=True)]
+    else:
+        rand_seed2 = [seed_list[i] for i in np.random.choice(len(seed_list), edge_num, replace=False)]
 
-    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=[accur_1])
-    model.summary()
+    # function pointer for gradient computation
+#    fn = gen_adv2_pytorch if sign else gen_adv3_pytorch
+#TODO: rework gen_adv3_pytorch
+    fn = gen_adv2_pytorch
+    
+    # select output neurons to compute gradient
+    interested_indice = np.random.choice(MAX_BITMAP_SIZE, edge_num)
+#    layer_list = [(layer.name, layer) for layer in model.layers]
 
+    with open('gradient_info_p', 'w') as f:
+        for idxx in range(len(interested_indice[:])):
+            # keras's would stall after multiple gradient compuation. Release memory and reload model to fix it.
+#            if idxx % 100 == 0:
+#                del model
+#                K.clear_session()
+#                model = build_model()
+#                model.load_weights('hard_label.h5')
+#                layer_list = [(layer.name, layer) for layer in model.layers]
+
+            print("number of feature " + str(idxx))
+            index = int(interested_indice[idxx])
+            fl = [rand_seed1[idxx], rand_seed2[idxx]]
+            adv_list = fn(index, fl, model, loss_func, idxx, 1)
+            tmp_list.append(adv_list)
+            for ele in adv_list:
+                ele0 = [str(el) for el in ele[0]]
+                ele1 = [str(int(el)) for el in ele[1]]
+#                ele1 = [np.array2string((el.astype('int32')), threshold=None) for el in ele[1]]
+                ele2 = ele[2]
+                f.write(",".join(ele0) + '|' + ",".join(ele1) + '|' + ele2 + "\n")
+
+
+
+def def_linear_model(input_dim, num_classes ):
+    model = nn.Sequential(
+    nn.Linear(in_features=input_dim, out_features=4096), 
+    nn.ReLU(inplace=True), 
+    nn.Linear(in_features=4096, out_features=num_classes), 
+    nn.Sigmoid()
+    
+    )
     return model
 
 
-def train(model):
-    loss_history = LossHistory()
-    lrate = keras.callbacks.LearningRateScheduler(step_decay)
-    callbacks_list = [loss_history, lrate]
-    model.fit_generator(train_generate(16),
-                        steps_per_epoch=(SPLIT_RATIO / 16 + 1),
-                        epochs=100,
-                        verbose=1, callbacks=callbacks_list)
-    # Save model and weights
-    model.save_weights("hard_label.h5")
+
+def build_model():
+    batch_size = 32
+    lr = 0.0001
+    num_classes = MAX_BITMAP_SIZE
+    epochs = 50
+
+    model = def_linear_model(input_dim=MAX_FILE_SIZE, num_classes=num_classes )
+    optimizer = torch.optim.Adam(model.parameters(), 
+                                          lr=lr, betas=(0.5, 0.999))
+#    model.add(Dense(4096, input_dim=MAX_FILE_SIZE))
+#    model.add(Activation('relu'))
+#    model.add(Dense(num_classes))
+#    model.add(Activation('sigmoid'))
+
+    #opt = keras.optimizers.adam(lr=0.0001)
+
+    #model.compile(loss='binary_crossentropy', optimizer=opt, metrics=[accur_1])
+    print(model)
+
+    return model ,  optimizer
+
+def fit(model, data,  optimizer,  criterion, epochs, lr , start_idx=1,  loss_history=[]):
+    model.train()
+
+
+    torch.cuda.empty_cache()
+    print("Lets's fit model")
+    # Losses & scores
+    
+    # Create optimizers
+    
+    for epoch in range(epochs):
+        print("Epoch ",  epoch)
+        print(type(data),  data)
+        i=0
+        for X, Y in data:
+            i=i+1 
+            if i > 100 :
+                break
+            #print(i)
+            #print(X, Y)
+            # Train model
+            # Clear model gradients
+            optimizer.zero_grad()
+            
+            # Pass data through model
+            X = torch.tensor(X).to(device)
+            Y = torch.tensor(Y).to(device)
+            preds = model(X)
+            
+            # Update discriminator weights
+            loss = criterion(preds, Y)
+            loss.backward()
+            
+            optimizer.step()
+            loss_history.append(loss.item())
+        
+
+
+            
+    return loss_history
+
+def train(model, optimizer, loss_func):
+#    loss_history = LossHistory()
+    loss_history = []
+    
+#    lrate = keras.callbacks.LearningRateScheduler(step_decay)
+#    callbacks_list = [loss_history, lrate]
+#    model.fit_generator(train_generate(16),
+#                        steps_per_epoch=(SPLIT_RATIO / 16 + 1),
+#                        epochs=100,
+#                        verbose=1, callbacks=callbacks_list)
+#    # Save model and weights
+    train_data = train_generate(64)
+    fit(model=model, data = train_data,  optimizer=optimizer,  criterion = loss_func, epochs =2, lr = 0.001 , start_idx=1,  loss_history=loss_history)
+    #model.save_weights("hard_label.h5")
+    torch.save(model, "hard_label.h5")
+
 
 
 def gen_grad(data):
+    global device
     global round_cnt
     t0 = time.time()
     process_data()
-    model = build_model()
-    train(model)
+    model,  optimizer = build_model()
+    loss_func = accur_1_pytorch
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    print(device)
+    model = model.to(device)
+    
+    train(model,  optimizer, loss_func)
     # model.load_weights('hard_label.h5')
-    gen_mutate2(model, 500, data[:5] == b"train")
+    gen_mutate2_pytorch(model, loss_func, 500, data[:5] == b"train")
     round_cnt = round_cnt + 1
     print(time.time() - t0)
 
 
 def setup_server():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((HOST, PORT))
-    sock.listen(1)
-    conn, addr = sock.accept()
-    print('connected by neuzz execution moduel ' + str(addr))
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    sock.bind((HOST, PORT))
+#    sock.listen(1)
+#    conn, addr = sock.accept()
+#    print('connected by neuzz execution moduel ' + str(addr))
     gen_grad(b"train")
+    print("Pretrain finished. Let's start!")
     conn.sendall(b"start")
     while True:
         data = conn.recv(1024)
